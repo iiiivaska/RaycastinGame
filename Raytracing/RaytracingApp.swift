ba42ced7657bd6e48c8dd2c325c1ca9303e5fe37
iiiivaska <iiiivaska@yandex.ru>
2023-03-19 21:37:13 +0300

    Initial Commit

---
+++ Raytracing/RaytracingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaytracingApp.swift
+//  Raytracing
+//
+//  Created by Василий Буланов on 19.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct RaytracingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
